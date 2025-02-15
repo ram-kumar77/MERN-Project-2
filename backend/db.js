4a2be04351c9225072d8da3ece8d759d76274bd7
Ram kumar <ramkumarsundar07@gmail.com>
2025-02-15 10:47:10 +0530

    Mongo db connection fix

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
 
 // Import and register models in correct order
 const Car = require('./models/carModel');
